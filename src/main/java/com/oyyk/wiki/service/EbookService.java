36fe0de8e5cb2fe5a9d1ebb4f30f6f2b203d4788
oyyk <745691616@qq.com>
2022-09-07 10:21:08 +0800

    3-7 封装请求参数和返回参数

--- src/main/java/com/oyyk/wiki/service/EbookService.java
+++ src/main/java/com/oyyk/wiki/service/EbookService.java
@@ -1,9 +1,13 @@
 package com.oyyk.wiki.service;
 
 import com.oyyk.wiki.domain.Ebook;
+import com.oyyk.wiki.req.EbookReq;
+import com.oyyk.wiki.resp.EbookResp;
 
 import java.util.List;
 
 public interface EbookService {
     public List<Ebook> list();
+
+    public List<EbookResp> list(EbookReq req);
 }
--- src/main/java/com/oyyk/wiki/service/impl/EbookServiceImpl.java
+++ src/main/java/com/oyyk/wiki/service/impl/EbookServiceImpl.java
@@ -1,11 +1,16 @@
 package com.oyyk.wiki.service.impl;
 
 import com.oyyk.wiki.domain.Ebook;
+import com.oyyk.wiki.domain.EbookExample;
 import com.oyyk.wiki.mapper.EbookMapper;
+import com.oyyk.wiki.req.EbookReq;
+import com.oyyk.wiki.resp.EbookResp;
 import com.oyyk.wiki.service.EbookService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,5 +22,21 @@ public class EbookServiceImpl implements EbookService {
     @Override
     public List<Ebook> list() {
         return ebookMapper.selectByExample(null);
+    }
+
+    @Override
+    public List<EbookResp> list(EbookReq req) {
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%"+req.getName()+"%");
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        List<EbookResp> respList = new ArrayList<>();
+        for (Ebook ebook : ebookList) {
+            EbookResp ebookResp = new EbookResp();
+            BeanUtils.copyProperties(ebook, ebookResp);
+            respList.add(ebookResp);
+        }
+        return respList;
     }
 }
